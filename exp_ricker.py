f69038973f573061d7a8fa8fa84f63fe602c811f
huangd5 <daolang.huang@aalto.fi>
2023-02-21 22:56:10 +0200

    Add prior mismatch support

--- exp_ricker.py
+++ exp_ricker.py
@@ -1,5 +1,5 @@
 from simulators.ricker import ricker
-
+import torch
 from networks.summary_nets import RickerSummary
 from utils.get_nn_models import *
 from inference.snpe.snpe_c import SNPE_C as SNPE
@@ -31,8 +31,10 @@ def main(args):
     if not os.path.exists(root_name):
         os.makedirs(root_name)
 
+    # prior = [Uniform(2 * torch.ones(1), 8 * torch.ones(1)),
+    #          Uniform(torch.zeros(1), 20 * torch.ones(1))]
     prior = [Uniform(2 * torch.ones(1), 8 * torch.ones(1)),
-             Uniform(torch.zeros(1), 20 * torch.ones(1))]
+             torch.distributions.log_normal.LogNormal(loc=torch.tensor([0.5]), scale=torch.tensor([1]))]
     simulator, prior = prepare_for_sbi(ricker, prior)
 
     sum_net = RickerSummary(input_size=1, hidden_dim=4).to(device)
@@ -46,11 +48,15 @@ def main(args):
 
     theta_gt = torch.tensor(theta_gt)
     obs = ricker(theta_gt).to(device)
-    sigma = torch.tensor(var)
-    obs_cont = corruption.magnitude_sigma(obs, var=sigma, length=100).reshape(-1, 100, 100)
+    # sigma = torch.tensor(var)
+    # obs_cont = corruption.magnitude_sigma(obs, var=sigma, length=100).reshape(-1, 100, 100)
+    # prior mismatch
+    obs_cont = ricker(torch.tensor([5, 25])).reshape(-1, 100, 100).to(device)
+
     # x_o_cont = torch.tensor(np.load("data/x_o_cont.npy"))
 
     theta, x = simulate_for_sbi(simulator, prior, num_simulations=num_simulations)
+
     x = x.reshape(num_simulations, 100, 100).to(device)
     theta = theta.to(device)
     density_estimator = inference.append_simulations(theta, x.unsqueeze(1)).train(corrupt_data_training="mmd", x_obs=obs_cont)
@@ -66,7 +72,7 @@ def main(args):
     torch.save(sum_net, root_name + "/sum_net.pkl")
     torch.save(density_estimator, root_name + "/density_estimator.pkl")
 
-    # with open(root_name + "/inference_{r}.pkl".format(r=str(i)), "wb") as handle:
+    # with open(root_name + "/inference.pkl", "wb") as handle:
     #     pickle.dump(inference, handle)
 
 
@@ -74,8 +80,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--beta", type=float, default=1)
     parser.add_argument("--seed", type=int, default="0")
-    parser.add_argument("--distance", type=str, default="mmd")
-    parser.add_argument("--num_simulations", type=int, default=1000)
+    parser.add_argument("--distance", type=str, default="none")
+    parser.add_argument("--num_simulations", type=int, default=50)
     parser.add_argument("--var", type=float, default=100)
     parser.add_argument("--theta", type=list, default=[4, 10])
     args = parser.parse_args()
