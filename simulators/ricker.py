57fe4f01883f96fd2b64c443146eec642025bd9b
huangd5 <daolang.huang@aalto.fi>
2023-04-13 16:47:23 +0300

    Update ricker model function

--- simulators/ricker.py
+++ simulators/ricker.py
@@ -2,25 +2,29 @@ import torch
 import numpy as np
 
 
-def ricker(theta):
-    logr = theta[0]
-    phi = theta[1]
-    sigma = 0.3
+class ricker():
+    def __init__(self, N=20):
+        self.N = N
 
-    N0 = 1
-    T = 100
-    nSamples = 100
+    def __call__(self, theta, *args, **kwargs):
+        logr = theta[0]
+        phi = theta[1]
+        sigma = 0.3
 
-    Y = torch.zeros(size=(nSamples, T))
+        N0 = 1
+        T = 100
+        nSamples = self.N
 
-    for i in range(nSamples):
-        et = torch.randn(T, )
-        Nt = torch.zeros(size=(T + 1,))
+        Y = torch.zeros(size=(nSamples, T))
 
-        Nt[0] = N0
+        for i in range(nSamples):
+            et = torch.randn(T, )
+            Nt = torch.zeros(size=(T + 1,))
 
-        for t in range(1, T + 1):
-            Nt[t] = torch.exp(logr + torch.log(Nt[t - 1]) - Nt[t - 1] + sigma * et[t - 1])
-            Y[i, t - 1] = torch.poisson(phi * Nt[t])
+            Nt[0] = N0
 
-    return Y
+            for t in range(1, T + 1):
+                Nt[t] = torch.exp(logr + torch.log(Nt[t - 1]) - Nt[t - 1] + sigma * et[t - 1])
+                Y[i, t - 1] = torch.poisson(phi * Nt[t])
+
+        return Y
